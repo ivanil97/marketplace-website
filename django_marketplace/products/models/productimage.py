5462c51ee5918316a53bfc227b731f790d5a5eaa
Максим Олесов <windigotp@gmail.com>
2024-08-19 14:02:40 +0300

    Added related_name to product in ProductImage

--- django_marketplace/products/models/productimage.py
+++ django_marketplace/products/models/productimage.py
@@ -5,6 +5,10 @@ from products.models.product import Product
 class ProductImage(models.Model):
     image = models.ImageField(
         null=True, blank=True,
-        upload_to="productimage_image_directory_path"
+        upload_to="productimage_image_directory_path",
     )
-    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    product = models.ForeignKey(
+        Product, 
+        on_delete=models.PROTECT, 
+        related_name='images',
+    )
