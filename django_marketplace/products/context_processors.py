37b4a7e85c1ff4743d550aa4ee09fb4a9ee6fb02
Пользователь <sashasergeevasus345@gmail.com>
2024-08-20 15:29:45 +0300

    Was changed "list_categories" from module context_processor.py. Function can upload Categories which has products and not archived

--- django_marketplace/products/context_processors.py
+++ django_marketplace/products/context_processors.py
@@ -1,8 +1,14 @@
+from django.db.models import Count
 from django.http import HttpRequest
 
 from .models import Category
 
 
 def list_categories(request: HttpRequest) -> dict:
-    categories: list[Category] = Category.objects.filter(archived=False)
+    categories: list[Category] = Category.objects.prefetch_related('children').annotate(
+        product_count=Count('products')
+    ).filter(
+        product_count__gt=0,
+        archived=False
+    )
     return {"list_categories": categories}
