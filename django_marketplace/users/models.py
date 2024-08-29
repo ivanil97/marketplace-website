b59957f4048a746a9f81e59dbbc3d609d046238e
aa_lozovoy <aalozzzovoy@gmail.com>
2024-08-29 23:32:58 +0300

    /s2t1_user_and_role_models

--- django_marketplace/users/models.py
+++ django_marketplace/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField()
+    name = models.CharField(max_length=150)
+    agreement_accepted = models.BooleanField(default=False)
