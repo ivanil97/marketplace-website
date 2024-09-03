9c5f7a79b6dfefc4622ec8e0172d04f4372e4692
Александр Гайдукевич <gajdukevich.sani@mail.ru>
2024-09-03 15:20:54 +0300

    change_review_service

--- django_marketplace/products/services/review_service.py
+++ django_marketplace/products/services/review_service.py
@@ -4,9 +4,8 @@ from django.shortcuts import get_object_or_404
 from django.core.paginator import Paginator
 
 
-def get_reviews_for_product(product_id, page_number=1, reviews_per_page=5):
-    product = get_object_or_404(Product, id=product_id)
-    reviews = product.reviews.order_by("-date")
+def get_reviews_for_product(page_number=1, reviews_per_page=5):
+    reviews = Review.objects.filter(product__id=id)
     paginator = Paginator(reviews, reviews_per_page)
     page_obj = paginator.get_page(page_number)
     return page_obj
