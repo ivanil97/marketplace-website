6e38979a97b2fe568affceca0253789958b2ebf7
Andrey Yudin <andr73@yahoo.com>
2024-09-06 00:45:11 +0300

    logout, register user & reset password form added.

--- django_marketplace/core/settings.py
+++ django_marketplace/core/settings.py
@@ -144,3 +144,10 @@ MEDIA_ROOT = BASE_DIR / 'uploads'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/user/account/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = str(os.getenv('EMAIL_USER'))
+EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_PASSWORD'))
--- django_marketplace/users/urls.py
+++ django_marketplace/users/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path, include
-from django.contrib.auth.views import LoginView
+from django.urls import path, include, reverse_lazy
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+
+)
+
 
 from .views.account import AccountView
+from .views.register import RegisterView
+from .views.password_reset import UserPasswordResetView
 
 app_name = "user"
 
@@ -12,5 +21,25 @@ urlpatterns = [
              redirect_authenticated_user=True,
          ),
          name="login"),
-    path('account/', AccountView.as_view(), name='account')
+    path('logout/',
+         LogoutView.as_view(
+             next_page=reverse_lazy("home"),
+         ),
+         name="logout"),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('account/', AccountView.as_view(), name='account'),
+    path('password-reset/', UserPasswordResetView.as_view(), name="password_reset"),
+    path(
+        'password-reset-confirm/<uidb64>/<token>/',
+        PasswordResetConfirmView.as_view(
+            template_name="templates_users/password_reset_confirm.html",
+            success_url=reverse_lazy("user:password_reset_complete"),
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        'password-reset-complete/',
+        PasswordResetCompleteView.as_view(template_name="templates_users/password_reset_complete.html"),
+        name="password_reset_complete"
+    ),
 ]
