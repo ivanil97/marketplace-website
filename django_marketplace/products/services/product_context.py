ecc393f586b694873c853a0ad472fe9e584fef96
Tihomirov Yuri <trafalgarlo2012@yandex.ru>
2024-09-08 10:37:01 +0300

    S2T7: correct context

--- django_marketplace/products/services/product_context.py
+++ django_marketplace/products/services/product_context.py
@@ -7,7 +7,9 @@ def product_context(product: Product, **kwargs) -> dict:
     description_full: list = product['description'].splitlines()
     description_short: list = description_full[:3]
     first_image = product.images[0]
-    reviews = get_reviews_for_product(**kwargs)
+    page_number = kwargs.get("page_number")
+    reviews_per_page = kwargs.get("reviews_per_page")
+    reviews = get_reviews_for_product(product_id=product.pk, page_number=page_number, reviews_per_page=reviews_per_page)
     context = {
         "description_full": description_full,
         "description_short": description_short,
