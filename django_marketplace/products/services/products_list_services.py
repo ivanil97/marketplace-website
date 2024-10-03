dd44ea301ef7302a50600221c99c136b5eb2fd4e
Tihomirov Yuri <trafalgarlo2012@yandex.ru>
2024-10-04 00:01:32 +0300

    correct filter and add services for products_list, annotate

---
+++ django_marketplace/products/services/products_list_services.py
@@ -0,0 +1,86 @@
+import enum
+from django.db.models import Avg, Count
+from products.models import Product
+from products.forms import SearchForm
+
+
+class ProductListEnum(enum.Enum):
+    POP_ASC = 'quantity_sold'
+    POP_DEC = '-quantity_sold'
+    PR_ASC = 'auto_seller_price'
+    PR_DEC = '-auto_seller_price'
+    REV_ASC = 'rev_count'
+    REV_DEC = '-rev_count'
+    DATE_ASC = 'created_at'
+    DATE_DEC = '-created_at'
+    NONE = '1'
+
+
+def filter_queryset(request, form=None):
+    """
+    Фильтрует набор продуктов на основе параметров запроса и формы.
+
+    :param request: HTTP-запрос.
+    :param form: Форма для фильтрации продуктов.
+    :return: Отфильтрованный набор продуктов.
+    """
+    queryset = Product.objects.prefetch_related(
+        "tags", "images", "seller_price", "features"
+    ).annotate(
+        auto_seller_price=Avg('seller_price__price'),
+        rev_count=Count('review')
+    )
+
+    category_id = request.GET.get('category')
+    curr_sort = request.GET.get('sort', 'auto_seller_price')
+
+    if form and form.is_valid():
+        price = form.cleaned_data.get('price')
+        title = form.cleaned_data.get('name')
+        in_stock = form.cleaned_data.get('in_stock')
+
+        if title:
+            queryset = queryset.filter(name__icontains=title)
+
+        if price:
+            price_range = price.split(';')
+            queryset = queryset.filter(
+                auto_seller_price__range=(price_range[0], price_range[1])
+            )
+
+        if in_stock:
+            queryset = queryset.filter(quantity_sold__gt=0)
+
+    if category_id:
+        queryset = queryset.filter(category_id=category_id)
+
+    if curr_sort != '1':
+        queryset = queryset.order_by(curr_sort)
+
+    return queryset.all()
+
+
+def get_context_data(request, products):
+    """
+    Формирует контекст для отображения списка продуктов.
+
+    :param request: HTTP-запрос.
+    :param products: Набор продуктов для отображения.
+    :return: Словарь с контекстом для шаблона.
+    """
+    curr_sort = request.GET.get('sort', 'auto_seller_price')
+    context = {
+        'products': products,
+        'POP_ASC': ProductListEnum.POP_ASC,
+        'POP_DEC': ProductListEnum.POP_DEC,
+        'PR_ASC': ProductListEnum.PR_ASC,
+        'PR_DEC': ProductListEnum.PR_DEC,
+        'REV_ASC': ProductListEnum.REV_ASC,
+        'REV_DEC': ProductListEnum.REV_DEC,
+        'DATE_ASC': ProductListEnum.DATE_ASC,
+        'DATE_DEC': ProductListEnum.DATE_DEC,
+        'NONE': ProductListEnum.NONE,
+        'curr_sort': curr_sort,
+        'form': SearchForm(request.POST or None),
+    }
+    return context
--- django_marketplace/products/views.py
+++ django_marketplace/products/views.py
@@ -21,6 +21,7 @@ from django.db.models import Count
 from django.db.models import Prefetch
 
 from comparisons.services.comparison_service import *
+from products.services.products_list_services import filter_queryset, get_context_data
 from products.services.review_service import add_review_to_product
 from products.services.viewed_products_service import ViewedProductsService
 
@@ -80,95 +81,47 @@ class ReviewCreateView(CreateView):
         return reverse_lazy('products:product_detail', kwargs={'slug': self.kwargs['slug']})
 
 
-class ProductListEnum(enum.Enum):
-    POP_ASC = 'quantity_sold'
-    POP_DEC = '-quantity_sold'
-    PR_ASC = 'auto_seller_price'
-    PR_DEC = '-auto_seller_price'
-    REV_ASC = 'rev_count'
-    REV_DEC = '-rev_count'
-    DATE_ASC = 'created_at'
-    DATE_DEC = '-created_at'
-    NONE = '1'
-
-
 class ProductsListView(ListView):
+    """
+    Представление для отображения списка продуктов.
+    """
+
     model = Product
     template_name = "templates_products/products_list.html"
     context_object_name = "products"
     paginate_by = 8
 
     def get_queryset(self):
-        form = SearchForm(self.request.POST)
-        if form.is_valid():
-            return self._filter_queryset(form)
+        """
+        Получает отфильтрованный набор продуктов на основе запроса.
+
+        :return: Отфильтрованный набор продуктов.
+        """
+        if self.request.method == 'POST':
+            form = SearchForm(self.request.POST)
+            if form.is_valid():
+                return filter_queryset(self.request, form)
+        return filter_queryset(self.request)
 
     def post(self, request, *args, **kwargs):
+        """
+        Обрабатывает POST-запрос.
+
+        :param request: HTTP-запрос.
+        :return: Ответ на запрос.
+        """
         return self.get(request, *args, **kwargs)
 
-    def _filter_queryset(self, form=None):
-        queryset = super().get_queryset().prefetch_related(
-            "tags", "images", "seller_price", "features"
-        ).annotate(
-            auto_seller_price=Avg('seller_price__price'),
-            rev_count=Count('review')
-        )
-        category_id = self.request.GET.get('category')
-        curr_sort = self.request.GET.get('sort', 'auto_seller_price')
-        if form and form.is_valid():
-            price = form.cleaned_data.get('price')
-            title = form.cleaned_data.get('name')
-            in_stock = form.cleaned_data.get('in_stock')
-            if title:
-                queryset = queryset.filter(name__icontains=title)
-            if price:
-                price_range = price.split(';')
-                queryset = queryset.filter(auto_seller_price__range=(price_range[0], price_range[1]))
-            if in_stock:
-                queryset = queryset.filter(quantity_sold__gt=0)
-        if category_id:
-            queryset = queryset.filter(category_id=category_id)
-        if curr_sort != '1':
-            queryset = queryset.order_by(curr_sort)
-        return queryset.all()
-
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        curr_sort = self.request.GET.get('sort', 'auto_seller_price')
-        context['POP_ASC'] = ProductListEnum.POP_ASC
-        context['POP_DEC'] = ProductListEnum.POP_DEC
-        context['PR_ASC'] = ProductListEnum.PR_ASC
-        context['PR_DEC'] = ProductListEnum.PR_DEC
-        context['REV_ASC'] = ProductListEnum.REV_ASC
-        context['REV_DEC'] = ProductListEnum.REV_DEC
-        context['DATE_ASC'] = ProductListEnum.DATE_ASC
-        context['DATE_DEC'] = ProductListEnum.DATE_DEC
-        context['NONE'] = ProductListEnum.NONE
-        context_new = {
-            'curr_sort': curr_sort,
-        }
-        context.update(context_new)
-        context['form'] = SearchForm(self.request.POST or None)
+        """
+        Получает контекст для шаблона.
 
+        :param kwargs: Дополнительные аргументы.
+        :return: Словарь с контекстом для шаблона.
+        """
+        products = self.get_queryset()
+        context = get_context_data(self.request, products)
         return context
-
-    def get_queryset(self):
-        category_id = self.request.GET.get('category', None)
-        curr_sort = self.request.GET.get('sort', 'auto_seller_price')
-        queryset = self.model.objects.prefetch_related(
-            "tags", "images", "seller_price", "features"
-        ).annotate(
-            auto_seller_price=Avg('seller_price__price')
-        ).annotate(
-            rev_count=Count('review')
-        )
-        if category_id:
-            queryset = queryset.filter(category_id=category_id)
-
-        if curr_sort != '1':
-            queryset = queryset.order_by(curr_sort)
-
-        return queryset.all()
 
 
 class ComparisonListView(View):
@@ -206,10 +159,8 @@ class HomeView(TemplateView):
         context['slider_banners'] = get_slider_banners()
         context['static_banners'] = get_static_banners()
         context['popular_items'] = get_popular_items()
-        limited_items = get_limited_items()
-        if limited_items:
-            context['limited_item_day'] = limited_items[0]
-            context['limited_items'] = limited_items[1]
+        context['limited_item_day'] = get_limited_items()[0]
+        context['limited_items'] = get_limited_items()[1]
         return context
 
 
@@ -217,4 +168,4 @@ class HomeView(TemplateView):
 def clear_cache(sender, instance, **kwargs):
     key = make_template_fragment_key("product_detail")
     cache.delete(key)
-    print(f"Cache cleared for key: {key}")
+    # print(f"Cache cleared for key: {key}")
