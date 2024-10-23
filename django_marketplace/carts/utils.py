756be0405a8c10f819a3946fdeafd72d0fa081c3
Пользователь <sashasergeevasus345@gmail.com>
2024-10-23 11:47:45 +0300

    Added Discounts for Cart

--- django_marketplace/carts/utils.py
+++ django_marketplace/carts/utils.py
@@ -1,8 +1,9 @@
 
 
-def solve_total_price(carts) -> int:
+def solve_total_price(carts) -> dict:
     """Дополнительная функция для подсчета полной стоимости всех товаров"""
     total_price = 0
+    total_quantity = 0
     for col in carts:
         if not col.get("deleted"): # Расчет полной стоимости идет в том случае если товар из сессии не был удален
             price, discount, quantity = col.get('price'), col.get('discount'), col.get('quantity')
@@ -10,8 +11,19 @@ def solve_total_price(carts) -> int:
                 p = round((price - price * discount[0].percent / 100) * col['quantity'], 2)
             else:
                 p = price * quantity
+
+            total_quantity += quantity
             total_price += p
-    return total_price
+
+    total_price = float(total_price)
+    if total_quantity > 5 and total_price > 500:
+        return {"total_price": round(total_price - (total_price * 0.1), 2), "total_quantity": total_quantity}
+    elif total_price > 500:
+        return {"total_price": round(total_price - (total_price * 0.05), 2), "total_quantity": total_quantity}
+    elif total_quantity > 5:
+        return {"total_price": round(total_price - (total_price * 0.05), 2), "total_quantity": total_quantity}
+
+    return {"total_price": total_price, "total_quantity": total_quantity}
 
 
 def prepare_data(request, carts) -> list[dict]:
