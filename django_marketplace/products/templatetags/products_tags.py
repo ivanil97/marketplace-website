74a41c171ce4ae759b28dc6fb0d8c5e0a8b9c07a
Пользователь <sashasergeevasus345@gmail.com>
2024-10-29 12:40:01 +0300

    Discounts bags for all project are fixed

--- django_marketplace/products/templatetags/products_tags.py
+++ django_marketplace/products/templatetags/products_tags.py
@@ -6,13 +6,13 @@ register = template.Library()
 @register.simple_tag()
 def get_discount(discounts):
     discount = max(discounts, key=lambda obj: obj.percent)
-    discount.from_date = str(discount.from_date)[5:11] + str(discount.to_date)[5:11]
+    mount = discount.to_date.strftime("%B")[:3]
+    discount.from_date = str(discount.to_date)[8:11] + mount
     return discount
 
 
 @register.simple_tag()
 def solve_price(price, discount):
-    price_old = price
-    p = int(price)
-    price_dis = round((p - p * discount.percent / 100), 2)
+    price_old = int(price)
+    price_dis = round((price_old - price_old * discount.percent / 100), 2)
     return {"old_price": price, "dis": price_dis}
